4121d5b56fbf12b70fac6d322c13f3a855e2c7a0
Daniel Ramirez <daniel.ramirezm@ingenieria.unam.edu>
2026-01-08 21:52:25 -0600

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Daniel Ramirez on 08/01/26.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
